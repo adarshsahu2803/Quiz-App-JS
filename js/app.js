de6127a9158ce269bc2ca8b43befbab94977a9de
Adarsh Sahu <adarshsahu1077@gmail.com>
2023-11-26 13:21:29 +0530

    Finished JS functions

--- js/app.js
+++ js/app.js
@@ -16,17 +16,64 @@ let questions = [{
     }]
 
 let index = 0
+let right = 0, wrong = 0
+let total = questions.length
 const quesBox = document.getElementById('quesBox')
 const optionItems = document.querySelectorAll('.options')
 
 function loadQuestion() {
-    let data = questions[index]
-    console.log(data)
+    if (index == total) {
+        return endQuiz();
+    }
+    reset();
+    const data = questions[index]
     quesBox.innerText = `${index+1}) ${data.que}`
     optionItems[0].nextElementSibling.innerText = data.a
     optionItems[1].nextElementSibling.innerText = data.b
     optionItems[2].nextElementSibling.innerText = data.c
     optionItems[3].nextElementSibling.innerText = data.d
+}
+
+function submitQuiz() {
+    const data = questions[index]
+    const ans = getAnswer()
+    if (ans === data.correct) {
+        right++;
+    }
+    else {
+        wrong++;
+    }
+    index++;
+    loadQuestion();
+    return;
+}
+
+function getAnswer() {
+    let res;
+    optionItems.forEach(
+        (input) => {
+            if (input.checked) {
+                res = input.value
+            }
+        }
+    )
+    return res;
+}
+
+function reset() {
+    optionItems.forEach(
+        (input) => {
+            input.checked = false;
+        }
+    )
+    return;
+}
+
+function endQuiz() {
+    document.getElementById("box").innerHTML = `
+        <h3> Thank you for playing the quiz! </h3>
+        <h2> ${right} / ${total} are correct </h2>
+    `
 }
 
 loadQuestion();
